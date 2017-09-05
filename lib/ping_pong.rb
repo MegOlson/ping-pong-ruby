7a98722229e4850f87c8ae751cc93c34f43b0d45
Christine Callo and Megan Olson <christine+megan@>
2017-09-05 09:56:06 -0700

    pass second test

--- lib/ping_pong.rb
+++ lib/ping_pong.rb
@@ -2,10 +2,18 @@ class Fixnum
   def ping_pong
     x = 0
     list = []
-    while (x < 2 )
+    while (x < self )
       x = x + 1
+      if x%15 == 0
+      list.push("ping-pong")
+      elsif x%5 == 0
+      list.push("pong")
+      elsif x%3 == 0
+      list.push("ping")
+      else
       list.push(x)
+      end
     end
-    list
+  list
   end
 end
--- spec/ping_pong_spec.rb
+++ spec/ping_pong_spec.rb
@@ -5,4 +5,8 @@ describe('#ping_pong') do
   it("counts up to inputted number in array") do
     expect((2).ping_pong()).to(eq([1,2]))
   end
+
+  it("replaces numbers divisible by 3 with 'ping', replaces numbers divisible by 5 with 'pong', replaces numbers divisible by 15 with 'ping-pong'") do
+    expect((5).ping_pong()).to(eq([1,2,"ping",4,"pong"]))
+  end
 end
